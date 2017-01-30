0fb94ed5b7fcc7449f5e85d84c1b26ae3cd3ed68
Sam van Herwaarden <samvherwaarden@protonmail.com>
2017-01-30 16:25:05 +0100

    Forgot to add articles.sql earlier

---
+++ db/articles.sql
@@ -0,0 +1,9 @@
+CREATE TABLE articles (
+    id             serial primary key
+  , pub_date       date not null
+  , title          text not null
+  , author         text not null
+  , url            text not null
+  , summary        text
+  , orig_lang      text not null
+);
