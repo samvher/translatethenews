156201c11567729516061c9a5597b1f75c3929e6
Sam van Herwaarden <samvherwaarden@protonmail.com>
2017-02-08 16:32:01 +0100

    Hooked up static middleware

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,7 @@ Author      : Sam van Herwaarden <samvherwaarden@protonmail.com>
 
 module Main where
 
-import TTN.ConnInfo           ( ttnConnInfo )
+import TTN.ConnInfo                   ( ttnConnInfo )
 import TTN.Routes
 
 import TTN.Controller.Article
@@ -14,28 +14,31 @@ import TTN.Controller.Core
 import TTN.Controller.User
 import TTN.Model.Core
 
+import Network.Wai.Middleware.Static  ( staticPolicy, addBase )
 import Web.Spock
 
 -- The web app
 
 -- | Connects paths, requests, handlers, authentication requirements
 app :: TTNMonad ()
-app = prehook initHook $ do
-        get listArticlesR listArticles
-        get root . redirect $ renderRoute loginR
-        prehook guestOnlyHook $ do
-            getpost registerR processRegistration
-            getpost loginR    processLogin
-        prehook authHook $ do
-            get     logoutR           processLogout
-            getpost newArticleR       newArticle
-            get     viewArticleR      viewArticle
-            getpost editArticleR      editArticle -- TODO: Maybe this should be disabled?
-            getpost newTranslationR   newTranslation
-            get     viewTranslationR  viewTranslation
+app = do middleware . staticPolicy $ addBase "/static"
+         prehook initHook $ do
+             get listArticlesR listArticles
+             get root . redirect $ renderRoute loginR
+             prehook guestOnlyHook $ do
+                 getpost registerR processRegistration
+                 getpost loginR    processLogin
+             prehook authHook $ do
+                 get     logoutR           processLogout
+                 getpost newArticleR       newArticle
+                 get     viewArticleR      viewArticle
+                 -- TODO: Maybe this should be disabled?
+                 getpost editArticleR      editArticle
+                 getpost newTranslationR   newTranslation
+                 get     viewTranslationR  viewTranslation
 
 -- | Configures and runs the Spock server
 main :: IO ()
 main = do cfg <- getCfg ttnConnInfo
-          runSpock 3000 (spock cfg app)
+          runSpock 3000 $ spock cfg app
 
